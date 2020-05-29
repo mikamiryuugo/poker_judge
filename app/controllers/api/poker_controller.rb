4bd321b114601a52c29e4bdaeb68aef4297ca66d
mikamiryu <mikamiryuugo@yahoo.co.jp>
2020-05-29 20:26:22 +0900

    [add]ストロングパラメーターを正しくセットした

--- app/controllers/api/poker_controller.rb
+++ app/controllers/api/poker_controller.rb
@@ -1,9 +1,25 @@
 class Api::PokerController < ApplicationController
   def judge
-    cards = Poker.new(params[:card_in_hand])
 
-    # render json: cards.errors.full_messages.to_json and return unless cards.valid?
+    cards = Poker.new(poker_params)
 
+    render json: cards.errors.full_messages and return unless cards.valid?
+
+    # cards.one_pair
     render json: cards
+
+    # なんの答えかをだすロジック作るのみかな。
+
   end
+
+
+private
+
+  def poker_params
+    params.require(:poker).permit(:card_in_hand)
+  end
+
+
+
+
 end
--- app/models/poker.rb
+++ app/models/poker.rb
@@ -3,8 +3,38 @@ class Poker < ApplicationRecord
 
 
   def check_format
-    # 正規表現
+    # 正規表現　この中で大体のエラーを防げそうかな。
     # cards.~~~
+  end
+
+  def one_pair
+    cards = self.split
+    cards.map! {|card| card.slice[0]}
+    cards.uniq.count == 3
+  end
+
+  def two_pair
+  end
+
+  def three_card
+  end
+
+  def four_card
+  end
+
+  def full_house
+  end
+
+  def flash
+  end
+
+  def straight
+  end
+
+  def straight_flash
+  end
+
+  def loyel_straight_flash
   end
 
 end
