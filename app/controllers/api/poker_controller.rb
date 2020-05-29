55cdf9bb03139c7bad1b00ffc17a4a740979edc4
mikamiryu <mikamiryuugo@yahoo.co.jp>
2020-05-29 18:39:50 +0900

    [add]CSRF対策追加

--- app/controllers/api/poker_controller.rb
+++ app/controllers/api/poker_controller.rb
@@ -1,6 +1,9 @@
 class Api::PokerController < ApplicationController
   def judge
     cards = Poker.new(params[:card_in_hand])
+
+    # render json: cards.errors.full_messages.to_json and return unless cards.valid?
+
     render json: cards
   end
 end
